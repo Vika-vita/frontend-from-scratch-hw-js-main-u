442c8bca1587e87305f1a7ac3f2897e7c457a16f
Vika-vita <vita.love7213@gmail.com>
2025-04-02 18:08:41 +0300

    nev commit

---
+++ lesson-01/01.js
@@ -0,0 +1,12 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20//
+// Результат присвойте переменной sum//
+
+// your code//
+let sum=0;
+let number=1;
+while (number<=20) {
+    sum+=number;
+    number++;
+}
+console.log("сумма чисел от 1 до 20:" + sum);
+
